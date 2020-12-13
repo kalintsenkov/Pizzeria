7d49a143d30743055b281e568fa22d73a1d468b4
Kalin Tsenkov <kalintsenkov3@gmail.com>
2020-12-13 20:36:15 +0200

    Added register logic

--- src/Client/src/app/authentication/register/register.component.ts
+++ src/Client/src/app/authentication/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { IErrors } from '../../core/interfaces/IErrors';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  isSubmitting = false;
+  registerForm: FormGroup;
+  errors: IErrors = { errors: {} };
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private authService: AuthService,
+  ) { }
 
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      email: [
+        { value: '', disabled: this.isSubmitting },
+        Validators.required
+      ],
+      password: [
+        { value: '', disabled: this.isSubmitting },
+        Validators.required,
+      ],
+      confirmPassword: [
+        { value: '', disabled: this.isSubmitting },
+        Validators.required,
+      ],
+    });
   }
 
+  register() {
+    this.isSubmitting = true;
+    this.authService.register(this.registerForm.value).subscribe(
+      res => {
+        this.router.navigateByUrl('/login');
+      },
+      err => {
+        this.errors = err;
+        this.isSubmitting = false;
+      }
+    );
+  }
 }
